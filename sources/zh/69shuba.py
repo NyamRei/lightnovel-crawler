46a546798fe40a59c6a618c27fbb849ea5cb5643
NyamRei <158081944+NyamRei@users.noreply.github.com>
2024-01-30 00:27:20 +0700

    Update 69xinshu.com
    The site has changed the domain

--- sources/zh/69shuba.py
+++ sources/zh/69shuba.py
@@ -12,9 +12,9 @@ headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.5",
     "Content-Type": "application/x-www-form-urlencoded",
-    "Origin": "https://www.69shuba.com",    # Updated to the new domain
+    "Origin": "https:/www.69xinshu.com",    # Updated to the new domain 
     "DNT": "1",
-    "Alt-Used": "www.69shuba.com",      # Updated to the new domain
+    "Alt-Used": "www.69xinshu.com",      # Updated to the new domain
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
     "Sec-Fetch-Dest": "document",
@@ -24,15 +24,15 @@ headers = {
 }
 
 logger = logging.getLogger(__name__)
-search_url = "https://www.69shuba.com/modules/article/search.php"    # Updated to the new domain
+search_url = "https://www.69xinshu.com/modules/article/search.php"    # Updated to the new domain
 
 
 class sixnineshu(Crawler):
-    base_url = ["https://www.69shuba.com/", "https://www.69shu.com/"]        # Updated to the new domain
+    base_url = ["https://www.69shuba.com/", "https://www.69shu.com/", "https://www.69xinshu.com/"]        # Updated to the new domain 
 
     def get_soup(self, url):
         """overwrite the get_soup function to set the encoding"""
-        data = requests.get(url, headers=headers)
+        data = requests.get(url, headers=headers)  
         data.encoding = "gbk"
         soup = BeautifulSoup(data.text, "html.parser")
         return soup
@@ -40,7 +40,7 @@ class sixnineshu(Crawler):
     def search_novel(self, query):
         query = urllib.parse.quote(query.encode("gbk"))
         data = requests.post(
-            "https://www.69shuba.com/modules/article/search.php",    # Updated to the new domain
+            "https://www.69xinshu.com/modules/article/search.php",    # Updated to the new domain
             headers=headers,
             data=f"searchkey={query}&searchtype=all",
         )
